4a819125a37568192f5217c8c5721f53772f3a70
Joel <jchinop@unsa.edu.pe>
2024-09-17 19:05:22 -0500

    fix table role

--- backend/src/main/resources/db/migration/V5__add_role_table.sql
+++ backend/src/main/resources/db/migration/V5__add_role_table.sql
@@ -4,5 +4,5 @@ CREATE TABLE roles (
   name VARCHAR(100) UNIQUE NOT NULL, -- Nombre del rol
   parent_role_id BIGINT, -- Para jerarquía de roles (opcional)
   createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Fecha de creación
-  updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Fecha de actualización
+  updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Fecha de actualización
 );
