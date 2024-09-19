f1b0cb9a269e08a257bb6951b8a20cdb7931387d
Joel <jchinop@unsa.edu.pe>
2024-09-18 22:33:05 -0500

    add search name or email

--- backend/src/main/java/com/devgarden/login_api/repository/UserRepository.java
+++ backend/src/main/java/com/devgarden/login_api/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.devgarden.login_api.repository;
 import com.devgarden.login_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsernameOrEmail(String username, String email);
 }
